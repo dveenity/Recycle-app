5b511ff961a16d5e6dea888cb0d73948bc968667
David Odion <davidodion898@gmail.com>
2024-04-13 04:31:58 +0100

    upload

--- recycle/src/Components/Custom/Logout.jsx
+++ recycle/src/Components/Custom/Logout.jsx
@@ -1,11 +1,14 @@
 import { useLogout } from "../Hooks/useLogout";
+import { useQueryClient } from "react-query"; // Import useQueryClient
 
 const Logout = () => {
+  const queryClient = useQueryClient(); // Access the queryClient instance
   const { logout } = useLogout();
 
-  const logOut = () => {
+  const logOut = async () => {
+    await queryClient.clear(); // Clear all cached data
     logout();
-    window.location.reload(); // Reload the page
+    window.location.reload(); // reload page
   };
 
   return (
